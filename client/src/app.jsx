77287fd43c91ea73cab14ec2f68ab4ff22173d0b
Danielxburns <danielburnsart@gmail.com>
2019-05-04 19:25:33 -0500

    initial commit

---
+++ client/src/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render () {
+    return (
+      <div>Let's play some Yahtzee!</div>
+    );
+  }
+}
+
+export default App;
